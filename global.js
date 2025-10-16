59c8a6067f5d55c1e2fcf9d49b73758c0d79ee56
JanieChan <janiej3.chan@gmail.com>
2025-10-16 00:56:17 -0700

    lab 3 nav links

--- global.js
+++ global.js
@@ -25,27 +25,27 @@ document.body.insertAdjacentHTML(
 // currentLink?.classList.add('current');
 
 let pages = [
-  { url: '/', title: 'Home' },
-  { url: '/projects/', title: 'Projects' },
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
   { url: 'https://github.com/JanieChan', title: 'Github'},
-  { url: '/contact/', title: 'Contact' },
-  { url: '/resume/', title: 'Resume' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
 ];
-
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/JanieChan_portfolio/";         // GitHub Pages repo name
+  
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
   let a = document.createElement('a');
   a.href = url;
   a.textContent = title;
   nav.append(a);
-  const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-  ? "/"                  // Local server
-  : "/JanieChan_portfolio/";         // GitHub Pages repo name
-  url = !url.startsWith('http') ? BASE_PATH + url : url;
   a.classList.toggle(
     'current',
     a.host === location.host && a.pathname === location.pathname,
